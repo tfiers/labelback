0e5db1c281d045f5eeba82e73feddea3d992ea6b
Tomas Fiers <tomas.fiers@gmail.com>
2018-03-30 00:42:52 +0200

    Allow CORS

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 let generateEvents = (num) => (
-	Array(num).fill().map((v,i)=>({id: i}))
+    Array(num).fill().map((v,i)=>({id: i}))
 )
 
 let generateNewState = () => ({
-	'events': {
+    'events': {
       'unlabelled': generateEvents(1000),
       'not_SWR': [],
       'SWR': [],
@@ -14,9 +14,16 @@ let generateNewState = () => ({
     },
 })
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers',
+               'Origin, X-Requested-With, Content-Type, Accept');
+    next()
+})
+
 app.get('/', (req, res) => res.json(generateNewState()))
 
 port = 3000
 app.listen(port, 
-	() => console.log(`Labelback app running on ${port}`)
+    () => console.log(`Labelback app running on ${port}`)
 )
